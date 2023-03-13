d446eb7abc04d2124e051e3ab859f47170b8da9e
GMC-Nickies <71288862+GMC-Nickies@users.noreply.github.com>
2023-03-12 21:05:08 -0400

    Update App.js

--- App.js
+++ App.js
@@ -15,12 +15,12 @@ import { Navigation } from "./src/infrastructure/navigation";
 import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDm17JVV-Q6vj32arwR-H1DPtyAU3_xoG4",
-  authDomain: "localgems-ecf6c.firebaseapp.com",
-  projectId: "localgems-ecf6c",
-  storageBucket: "localgems-ecf6c.appspot.com",
-  messagingSenderId: "396846805508",
-  appId: "1:396846805508:web:b4631235a744e20d59bb51"
+  apiKey: "yourKey",
+  authDomain: "yourDomain",
+  projectId: "yourID",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 initializeApp(firebaseConfig);
