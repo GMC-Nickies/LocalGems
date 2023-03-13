7e776d462e85394cc18dae1049527436786d4ab8
GMC-Nickies <71288862+GMC-Nickies@users.noreply.github.com>
2023-03-12 21:04:03 -0400

    Update env.js

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,7 +1,7 @@
 import { Platform } from "react-native";
 
-const liveHost = "https://us-central1-localgems-ecf6c.cloudfunctions.net";
-const localHost = "http://127.0.0.1:5001/localgems-ecf6c/us-central1";
+const liveHost = "yourHost";
+const localHost = "yourHost";
 
 export const isAndroid = Platform.OS === "android";
 export const isDevelopment = process.env.NODE_ENV === "development";
